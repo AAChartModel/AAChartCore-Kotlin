5758abbad52220eda4fe907eeddc93ab6b7d0b69
An An <2236368544@qq.com>
2019-06-22 19:39:45 +0800

    Enhance AAGradientColor

--- app/src/main/java/com/aachartmodel/aainfographics/AAInfographicsLib/AATools/AAGradientColor.kt
+++ app/src/main/java/com/aachartmodel/aainfographics/AAInfographicsLib/AATools/AAGradientColor.kt
@@ -1,115 +1,130 @@
 package com.example.chartcorekotlin.AAChartConfiger
 
 import java.util.HashMap
+enum class AALinearGradientDirection {
+    ToTop,           //⇧⇧⇧⇧⇧⇧
+    ToBottom,        //⇩⇩⇩⇩⇩⇩
+    ToLeft,          //⇦⇦⇦⇦⇦⇦
+    ToRight,         //⇨⇨⇨⇨⇨⇨
+    ToTopLeft,       //⇖⇖⇖⇖⇖⇖
+    ToTopRight,      //⇗⇗⇗⇗⇗⇗
+    ToBottomLeft,    //⇙⇙⇙⇙⇙⇙
+    ToBottomRight,   //⇘⇘⇘⇘⇘⇘
+}
 
 
 object AAGradientColor {
 
 
-    fun oceanBlueColor(): HashMap<String, Any> {
-        return configureGradientColor("#2E3192", "#1BFFFF")
+    fun oceanBlueColor(): Map<String, Any> {
+        return gradientColorMap("#2E3192", "#1BFFFF")
     }
 
-    fun sanguineColor(): HashMap<String, Any> {
-        return configureGradientColor("#D4145A", "#FBB03B")
+    fun sanguineColor(): Map<String, Any> {
+        return gradientColorMap("#D4145A", "#FBB03B")
     }
 
-    fun lusciousLimeColor(): HashMap<String, Any> {
-        return configureGradientColor("#009245", "#FCEE21")
+    fun lusciousLimeColor(): Map<String, Any> {
+        return gradientColorMap("#009245", "#FCEE21")
     }
 
-    fun purpleLakeColor(): HashMap<String, Any> {
-        return configureGradientColor("#662D8C", "#ED1E79")
+    fun purpleLakeColor(): Map<String, Any> {
+        return gradientColorMap("#662D8C", "#ED1E79")
     }
 
-    fun freshPapayaColor(): HashMap<String, Any> {
-        return configureGradientColor("#ED1C24", "#FCEE21")
+    fun freshPapayaColor(): Map<String, Any> {
+        return gradientColorMap("#ED1C24", "#FCEE21")
     }
 
-    fun ultramarineColor(): HashMap<String, Any> {
-        return configureGradientColor("#00A8C5", "#FFFF7E")
+    fun ultramarineColor(): Map<String, Any> {
+        return gradientColorMap("#00A8C5", "#FFFF7E")
     }
 
-    fun pinkSugarColor(): HashMap<String, Any> {
-        return configureGradientColor("#D74177", "#FFE98A")
+    fun pinkSugarColor(): Map<String, Any> {
+        return gradientColorMap("#D74177", "#FFE98A")
     }
 
-    fun lemonDrizzleColor(): HashMap<String, Any> {
-        return configureGradientColor("#FB872B", "#D9E021")
+    fun lemonDrizzleColor(): Map<String, Any> {
+        return gradientColorMap("#FB872B", "#D9E021")
     }
 
-    fun victoriaPurpleColor(): HashMap<String, Any> {
-        return configureGradientColor("#312A6C", "#852D91")
+    fun victoriaPurpleColor(): Map<String, Any> {
+        return gradientColorMap("#312A6C", "#852D91")
     }
 
-    fun springGreensColor(): HashMap<String, Any> {
-        return configureGradientColor("#009E00", "#FFFF96")
+    fun springGreensColor(): Map<String, Any> {
+        return gradientColorMap("#009E00", "#FFFF96")
     }
 
-    fun mysticMauveColor(): HashMap<String, Any> {
-        return configureGradientColor("#B066FE", "#63E2FF")
+    fun mysticMauveColor(): Map<String, Any> {
+        return gradientColorMap("#B066FE", "#63E2FF")
     }
 
-    fun reflexSilverColor(): HashMap<String, Any> {
-        return configureGradientColor("#808080", "#E6E6E6")
+    fun reflexSilverColor(): Map<String, Any> {
+        return gradientColorMap("#808080", "#E6E6E6")
     }
 
-    fun neonGlowColor(): HashMap<String, Any> {
-        return configureGradientColor("#00FFA1", "#00FFFF")
+    fun neonGlowColor(): Map<String, Any> {
+        return gradientColorMap("#00FFA1", "#00FFFF")
     }
 
-    fun berrySmoothieColor(): HashMap<String, Any> {
-        return configureGradientColor("#8E78FF", "#FC7D7B")
+    fun berrySmoothieColor(): Map<String, Any> {
+        return gradientColorMap("#8E78FF", "#FC7D7B")
     }
 
-    fun newLeafColor(): HashMap<String, Any> {
-        return configureGradientColor("#00537E", "#3AA17E")
+    fun newLeafColor(): Map<String, Any> {
+        return gradientColorMap("#00537E", "#3AA17E")
     }
 
-    fun cottonCandyColor(): HashMap<String, Any> {
-        return configureGradientColor("#FCA5F1", "#B5FFFF")
+    fun cottonCandyColor(): Map<String, Any> {
+        return gradientColorMap("#FCA5F1", "#B5FFFF")
     }
 
-    fun pixieDustColor(): HashMap<String, Any> {
-        return configureGradientColor("#D585FF", "#00FFEE")
+    fun pixieDustColor(): Map<String, Any> {
+        return gradientColorMap("#D585FF", "#00FFEE")
     }
 
-    fun fizzyPeachColor(): HashMap<String, Any> {
-        return configureGradientColor("#F24645", "#EBC08D")
+    fun fizzyPeachColor(): Map<String, Any> {
+        return gradientColorMap("#F24645", "#EBC08D")
     }
 
-    fun sweetDreamColor(): HashMap<String, Any> {
-        return configureGradientColor("#3A3897", "#A3A1FF")
+    fun sweetDreamColor(): Map<String, Any> {
+        return gradientColorMap("#3A3897", "#A3A1FF")
     }
 
-    fun firebrickColor(): HashMap<String, Any> {
-        return configureGradientColor("#45145A", "#FF5300")
+    fun firebrickColor(): Map<String, Any> {
+        return gradientColorMap("#45145A", "#FF5300")
     }
 
-    fun wroughtIronColor(): HashMap<String, Any> {
-        return configureGradientColor("#333333", "#5A5454")
+    fun wroughtIronColor(): Map<String, Any> {
+        return gradientColorMap("#333333", "#5A5454")
     }
 
-    fun deepSeaColor(): HashMap<String, Any> {
-        return configureGradientColor("#4F00BC", "#29ABE2")
+    fun deepSeaColor(): Map<String, Any> {
+        return gradientColorMap("#4F00BC", "#29ABE2")
     }
 
-    fun coastalBreezeColor(): HashMap<String, Any> {
-        return configureGradientColor("#00B7FF", "#FFFFC7")
+    fun coastalBreezeColor(): Map<String, Any> {
+        return gradientColorMap("#00B7FF", "#FFFFC7")
     }
 
-    fun eveningDelightColor(): HashMap<String, Any> {
-        return configureGradientColor("#93278F", "#00A99D")
+    fun eveningDelightColor(): Map<String, Any> {
+        return gradientColorMap("#93278F", "#00A99D")
     }
 
-    private fun configureGradientColor(startColor: String,
-                                       endColor: String): HashMap<String, Any> {
-        val linearGradientColorMap = mapOf(
-            "x1" to 0,
-            "y1" to 1,
-            "x2" to 0,
-            "y2" to 0
-        )
+    private fun gradientColorMap(startColor: String,
+                                 endColor: String): HashMap<String, Any> {
+        return gradientColorMap(
+            AALinearGradientDirection.ToTop,
+            startColor,
+            endColor)
+    }
+
+    private fun gradientColorMap(
+        direction: AALinearGradientDirection,
+        startColor: String,
+        endColor: String): HashMap<String, Any> {
+        val linearGradientColorMap = linearGradientMap(direction)
         val stopsArr = arrayOf<Any>(
             arrayOf(0, startColor),
             arrayOf(1, endColor)
@@ -118,5 +133,36 @@ object AAGradientColor {
         gradientColorMap["linearGradient"] = linearGradientColorMap
         gradientColorMap["stops"] = stopsArr
         return gradientColorMap
+    }
+
+    /**
+    (0,0) ----------- (1,0)
+    |                   |
+    |                   |
+    |                   |
+    |                   |
+    |                   |
+    (0,1) ----------- (1,1)
+     */
+    private fun linearGradientMap(direction: AALinearGradientDirection):Map<String,Int> {
+        when (direction) {
+            AALinearGradientDirection.ToTop ->
+                return mapOf("x1" to 0, "y1" to 1, "x2" to 0, "y2" to 0)
+            AALinearGradientDirection.ToBottom ->
+                return mapOf("x1" to 0, "y1" to 0, "x2" to 0, "y2" to 1)
+            AALinearGradientDirection.ToLeft ->
+                return mapOf("x1" to 1, "y1" to 0, "x2" to 0, "y2" to 0)
+            AALinearGradientDirection.ToRight ->
+                return mapOf("x1" to 0, "y1" to 0, "x2" to 1, "y2" to 0)
+            AALinearGradientDirection.ToTopLeft ->
+                return mapOf("x1" to 1, "y1" to 1, "x2" to 0, "y2" to 0)
+            AALinearGradientDirection.ToTopRight ->
+                return mapOf("x1" to 0, "y1" to 1, "x2" to 1, "y2" to 0)
+            AALinearGradientDirection.ToBottomLeft ->
+                return mapOf("x1" to 1, "y1" to 0, "x2" to 0, "y2" to 1)
+            AALinearGradientDirection.ToBottomRight ->
+                return mapOf("x1" to 0, "y1" to 0, "x2" to 1, "y2" to 1)
+        }
+
     }
 }
