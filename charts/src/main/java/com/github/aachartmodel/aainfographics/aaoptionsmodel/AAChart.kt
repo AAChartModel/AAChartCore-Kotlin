6e93704ee4c861c4a08500b69ad7973f06362d04
AnAn <2236368544@qq.com>
2021-01-26 11:11:57 +0800

    Add AAResetZoomButton for AAChart

--- charts/src/main/java/com/github/aachartmodel/aainfographics/aaoptionsmodel/AAChart.kt
+++ charts/src/main/java/com/github/aachartmodel/aainfographics/aaoptionsmodel/AAChart.kt
@@ -12,25 +12,26 @@ import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartZoomType
 
 class AAChart {
-    private var type: String? = null
-    private var backgroundColor: Any? = null
-    private var plotBackgroundImage: String? = null
-    private var pinchType: String? = null
-    private var panning: Boolean? = null
-    private var panKey: String? = null
-    private var polar: Boolean? = null
-    private var animation: AAAnimation? = null
-    private var inverted: Boolean? = null
+    var type: String? = null
+    var backgroundColor: Any? = null
+    var plotBackgroundImage: String? = null
+    var pinchType: String? = null
+    var panning: Boolean? = null
+    var panKey: String? = null
+    var polar: Boolean? = null
+    var animation: AAAnimation? = null
+    var inverted: Boolean? = null
 
     /*图表外边缘和绘图区域之间的边距。 数组中的数字分别表示顶部，右侧，底部和左侧 ([👆,👉,👇,👈])。
     也可以使用 marginTop，marginRight，marginBottom 和 marginLeft 来设置某一个方向的边距*/
-    private var margin: Array<Float>? = null
-    private var marginTop: Float? = null//👆
-    private var marginRight: Float? = null//👉
-    private var marginBottom: Float? = null//👇
-    private var marginLeft: Float? = null//👈
+    var margin: Array<Float>? = null
+    var marginTop: Float? = null//👆
+    var marginRight: Float? = null//👉
+    var marginBottom: Float? = null//👇
+    var marginLeft: Float? = null//👈
     var scrollablePlotArea: AAScrollablePlotArea? = null
-
+    var resetZoomButton: AAResetZoomButton? = null
+    
 
     fun type(prop: AAChartType?): AAChart {
         type = prop?.value
@@ -116,5 +117,31 @@ class AAChart {
         scrollablePlotArea = prop
         return this
     }
+    
+    fun resetZoomButton(prop: AAResetZoomButton): AAChart? {
+        resetZoomButton = prop
+        return this
+    }
 
+}
+
+
+class AAResetZoomButton {
+    var position: AAPosition? = null
+    var relativeTo: String? = null
+    var theme: Map<String, Any>? = null
+    fun position(prop: AAPosition?): AAResetZoomButton {
+        position = prop
+        return this
+    }
+
+    fun relativeTo(prop: String?): AAResetZoomButton {
+        relativeTo = prop
+        return this
+    }
+
+    fun theme(prop: Map<String, Any>?): AAResetZoomButton {
+        theme = prop
+        return this
+    }
 }
