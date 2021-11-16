066eac68648319d40dc873de46ab549e1b14c406
An An <2236368544@qq.com>
2021-11-16 14:37:15 +0800

    Fix #99 => Set pie chart colors theme

--- sample/src/main/java/com/github/aachartmodel/aainfographics/demo/chartcomposer/SpecialChartComposer.kt
+++ sample/src/main/java/com/github/aachartmodel/aainfographics/demo/chartcomposer/SpecialChartComposer.kt
@@ -67,6 +67,7 @@ object SpecialChartComposer {
             .backgroundColor("#ffffff")
             .title("LANGUAGE MARKET SHARES JANUARY,2020 TO MAY")
             .subtitle("virtual data")
+            .colorsTheme(arrayOf("#0c9674", "#7dffc0", "#d11b5f", "#facd32", "#ffffa0",))
             .dataLabelsEnabled(true)//是否直接显示扇形图数据
             .yAxisTitle("℃")
             .series(arrayOf(
