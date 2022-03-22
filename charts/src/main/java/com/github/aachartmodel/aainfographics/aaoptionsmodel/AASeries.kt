84e3a435e5058ea3d035b3dbf5c240558e560809
An An <2236368544@qq.com>
2022-03-22 14:42:12 +0800

    Update AASeries.kt

--- charts/src/main/java/com/github/aachartmodel/aainfographics/aaoptionsmodel/AASeries.kt
+++ charts/src/main/java/com/github/aachartmodel/aainfographics/aaoptionsmodel/AASeries.kt
@@ -12,7 +12,16 @@ import com.github.aachartmodel.aainfographics.aachartcreator.AAChartStackingType
 import com.github.aachartmodel.aainfographics.aachartcreator.AAShadow
 
 class AASeries {
-    var borderRadius: Float? = null
+    var borderColor //The border color, It is only valid for column, bar, pie, columnrange, pyramid and funnel chart types
+            : String? = null
+    var borderWidth //The border width, It is only valid for column, bar, pie, columnrange, pyramid and funnel chart types
+            : Float? = null
+    var borderRadius //The corner radius of the border surrounding each column or bar.
+            : Float? = null
+    var borderRadiusTopLeft: Any? = null
+    var borderRadiusTopRight: Any? = null
+    var borderRadiusBottomLeft: Any? = null
+    var borderRadiusBottomRight: Any? = null
     var marker: AAMarker? = null
     var stacking: String? = null
     var animation: AAAnimation? = null
@@ -23,8 +32,38 @@ class AASeries {
     var shadow: AAShadow? = null
     var dataLabels: AADataLabels? = null
 
+    fun borderColor(prop: String?): AASeries {
+        borderColor = prop
+        return this
+    }
+
+    fun borderWidth(prop: Float?): AASeries {
+        borderWidth = prop
+        return this
+    }
+
     fun borderRadius(prop: Float?): AASeries {
         borderRadius = prop
+        return this
+    }
+
+    fun borderRadiusTopLeft(prop: Any?): AASeries {
+        borderRadiusTopLeft = prop
+        return this
+    }
+
+    fun borderRadiusTopRight(prop: Any?): AASeries {
+        borderRadiusTopRight = prop
+        return this
+    }
+
+    fun borderRadiusBottomLeft(prop: Any?): AASeries {
+        borderRadiusBottomLeft = prop
+        return this
+    }
+
+    fun borderRadiusBottomRight(prop: Any?): AASeries {
+        borderRadiusBottomRight = prop
         return this
     }
 
