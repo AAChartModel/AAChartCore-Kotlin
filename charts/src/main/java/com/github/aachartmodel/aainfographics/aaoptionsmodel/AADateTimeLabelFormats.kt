e3d353d1ac9b92a8864a522612f011530f21ed15
An An <2236368544@qq.com>
2022-04-14 17:06:36 +0800

    Create AADateTimeLabelFormats.kt

---
+++ charts/src/main/java/com/github/aachartmodel/aainfographics/aaoptionsmodel/AADateTimeLabelFormats.kt
@@ -0,0 +1,52 @@
+package com.github.aachartmodel.aainfographics.aaoptionsmodel
+
+class AADateTimeLabelFormats {
+    var millisecond: String? = null
+    var second: String? = null
+    var minute: String? = null
+    var hour: String? = null
+    var day: String? = null
+    var week: String? = null
+    var month: String? = null
+    var year: String? = null
+
+    fun millisecond(prop: String?): AADateTimeLabelFormats {
+        millisecond = prop
+        return this
+    }
+
+    fun second(prop: String?): AADateTimeLabelFormats {
+        second = prop
+        return this
+    }
+
+    fun minute(prop: String?): AADateTimeLabelFormats {
+        minute = prop
+        return this
+    }
+
+    fun hour(prop: String?): AADateTimeLabelFormats {
+        hour = prop
+        return this
+    }
+
+    fun day(prop: String?): AADateTimeLabelFormats {
+        day = prop
+        return this
+    }
+
+    fun week(prop: String?): AADateTimeLabelFormats {
+        week = prop
+        return this
+    }
+
+    fun month(prop: String?): AADateTimeLabelFormats {
+        month = prop
+        return this
+    }
+
+    fun year(prop: String?): AADateTimeLabelFormats {
+        year = prop
+        return this
+    }
+}
