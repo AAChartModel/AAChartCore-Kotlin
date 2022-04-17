4ff0c6d03d794d22a89321d0d038e4eb999c9dd3
An An <2236368544@qq.com>
2022-04-17 19:53:08 +0800

    Refactor AAXAxis and AAYAxis extend AAAxis

--- charts/src/main/java/com/github/aachartmodel/aainfographics/aaoptionsmodel/AAYAxis.kt
+++ charts/src/main/java/com/github/aachartmodel/aainfographics/aaoptionsmodel/AAYAxis.kt
@@ -8,55 +8,26 @@
  */
 package com.github.aachartmodel.aainfographics.aaoptionsmodel
 
-class AAYAxis {
-    var alternateGridColor: Any? = null
-    var title: AATitle? = null
-    var type: String? = null
-    var dateTimeLabelFormats: AADateTimeLabelFormats? = null
-    var plotBands: Array<AAPlotBandsElement>? = null
-    var plotLines: Array<AAPlotLinesElement>? = null
-    var categories: Array<String>? = null
-    var reversed: Boolean? = null
-    var gridLineWidth: Float? = null// y 轴网格线宽度
-    var gridLineColor: String? = null  // y 轴网格线颜色
-    var gridLineDashStyle: String? = null //网格线线条样式，所有可用的线条样式参考：Highcharts线条样式
-    var gridLineInterpolation: String? =
-        null //Polar charts only. Whether the grid lines should draw as a polygon with straight lines between categories, or as circles. Can be either circle or polygon. 默认是：null.
-    var labels: AALabels? = null//用于设置 y 轴文字相关的
-    var lineWidth: Float? = null // y 轴线宽度
-    var lineColor: String? = null // y 轴线颜色
-    var off: Float? = null // y 轴线水平偏移
-    var allowDecimals: Boolean? = null //y轴是否允许显示小数
-    var max: Float? = null //y轴最大值
-    var min: Float? = null //y轴最小值（设置为0就不会有负数）
-    var minRange: Int? = null
-    var minTickInterval: Int? = null //The minimum tick interval allowed in axis values. For example on zooming in on an axis with daily data, this can be used to prevent the axis from showing hours. Defaults to the closest distance between two points on the axis.
-    var minorGridLineColor: String? = null //Color of the minor, secondary grid lines.
-    var minorGridLineDashStyle: String? = null //The dash or dot style of the minor grid lines.
-    var minorGridLineWidth: Float? = null //Width of the minor, secondary grid lines.
-    var minorTickColor: String? = null //Color for the minor tick marks.
-    var minorTickInterval: Any? = null /*Specific tick interval in axis units for the minor ticks. On a linear axis, if "auto", the minor tick interval is calculated as a fifth of the tickInterval. If null or undefined, minor ticks are not shown.
-     On logarithmic axes, the unit is the power of the value. For example, setting the minorTickInterval to 1 puts one tick on each of 0.1, 1, 10, 100 etc. Setting the minorTickInterval to 0.1 produces 9 ticks between 1 and 10, 10 and 100 etc.
-    If user settings dictate minor ticks to become too dense, they don't make sense, and will be ignored to prevent performance problems.*/
-    var minorTickLength: Float? = null //The pixel length of the minor tick marks.
-    var minorTickPosition: String? = null //The position of the minor tick marks relative to the axis line. Can be one of inside and outside. Defaults to outside.
-    var minorTickWidth: Float? = null //The pixel width of the minor tick mark.
+class AAYAxis: AAAxis() {
+    var stackLabels: Any? = null
+    fun stackLabels(prop: Any?): AAYAxis {
+        stackLabels = prop
+        return this
+    }
+//////////////////////////////////////////////
 
-    // var minPadding:   //Padding of the min value relative to the length of the axis. A padding of 0.05 will make a 100px axis 5px longer. This is useful when you don't want the lowest data value to appear on the edge of the plot area. 默认是：0.05.
-    var tickPositions: Array<Any>? = null //自定义Y轴坐标（如：[0, 25, 50, 75 , 100]）
-    var visible: Boolean? = null //y轴是否允许显示
-    var opposite: Boolean? =
-        null//是否将坐标轴显示在对立面，默认情况下 x 轴是在图表的下方显示，y 轴是在左方，坐标轴显示在对立面后，x 轴是在上方显示，y 轴是在右方显示（即坐标轴会显示在对立面）。该配置一般是用于多坐标轴区分展示，另外在 Highstock 中，y 轴默认是在对立面显示的。 默认是：false.
-    var tickInterval: Int? = null
-    var crosshair: AACrosshair? = null  //准星线样式设置
-    var stackLabels: String? = null
-    var tickWidth: Float? = null//坐标轴刻度线的宽度，设置为 0 时则不显示刻度线
-    var tickLength: Float? = null//坐标轴刻度线的长度。 默认是：10.
-    var tickPosition: String? =
-        null //刻度线相对于轴线的位置，可用的值有 inside 和 outside，分别表示在轴线的内部和外部。 默认是：outside.
+    fun allowDecimals(prop: Boolean?): AAYAxis {
+        allowDecimals = prop
+        return this
+    }
 
     fun alternateGridColor(prop: Any): AAYAxis {
         alternateGridColor = prop
+        return this
+    }
+
+    fun crosshair(prop: AACrosshair): AAYAxis {
+        crosshair = prop
         return this
     }
 
@@ -85,8 +56,13 @@ class AAYAxis {
         return this
     }
 
-    fun categories(prop: Array<String>): AAYAxis {
+    fun categories(prop: Array<String>?): AAYAxis {
         categories = prop
+        return this
+    }
+
+    fun linkedTo(prop: Int?): AAYAxis {
+        linkedTo = prop
         return this
     }
 
@@ -132,11 +108,6 @@ class AAYAxis {
 
     fun off(prop: Float?): AAYAxis {
         off = prop
-        return this
-    }
-
-    fun allowDecimals(prop: Boolean?): AAYAxis {
-        allowDecimals = prop
         return this
     }
 
@@ -200,13 +171,13 @@ class AAYAxis {
         return this
     }
 
-    fun tickPositions(prop: Array<Any>): AAYAxis {
-        tickPositions = prop
+    fun visible(prop: Boolean?): AAYAxis {
+        visible = prop
         return this
     }
 
-    fun visible(prop: Boolean?): AAYAxis {
-        visible = prop
+    fun startOnTick(prop: Boolean?): AAYAxis {
+        startOnTick = prop
         return this
     }
 
@@ -215,18 +186,23 @@ class AAYAxis {
         return this
     }
 
+    fun tickColor(prop: String?): AAYAxis {
+        tickColor = prop
+        return this
+    }
+
+    fun tickPositions(prop: Array<Any>): AAYAxis {
+        tickPositions = prop
+        return this
+    }
+
     fun tickInterval(prop: Int?): AAYAxis {
         tickInterval = prop
         return this
     }
 
-    fun crosshair(prop: AACrosshair): AAYAxis {
-        crosshair = prop
-        return this
-    }
-
-    fun stackLabels(prop: String): AAYAxis {
-        stackLabels = prop
+    fun tickmarkPlacement(prop: String): AAYAxis {
+        tickmarkPlacement = prop
         return this
     }
 
