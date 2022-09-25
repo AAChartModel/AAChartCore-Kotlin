cd1b2a0b24dd177ebb6886ea95f4fae8ffa8c616
An An <2236368544@qq.com>
2022-09-25 15:56:32 +0800

    Update AALabel.kt

--- charts/src/main/java/com/github/aachartmodel/aainfographics/aaoptionsmodel/AALabel.kt
+++ charts/src/main/java/com/github/aachartmodel/aainfographics/aaoptionsmodel/AALabel.kt
@@ -8,6 +8,9 @@
  */
 package com.github.aachartmodel.aainfographics.aaoptionsmodel
 
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartAlignType
+import com.github.aachartmodel.aainfographics.aatools.AAJSStringPurer
+
 class AALabel {
     var align: String? = null //标签的对齐方式，可用的值有 "left"、"center" 及 "right"。默认值是根据坐标轴的位置（在图表中的位置）即标签的旋转角度进行智能判断的。 默认是：center.
     var rotation: Number? = null //标签的旋转角度 默认是：0.
@@ -18,6 +21,8 @@ class AALabel {
     var style: Any? = null //轴标签的 CSS 样式
     var x: Number? = null //水平偏移
     var y: Number? = null // 竖直偏移
+    var format: String? = null //格式化标签文本
+    var formatter: String? = null //格式化标签文本
 
 
     fun align(prop: String?): AALabel {
@@ -35,8 +40,8 @@ class AALabel {
         return this
     }
 
-    fun textAlign(prop: String?): AALabel {
-        textAlign = prop
+    fun textAlign(prop: AAChartAlignType?): AALabel {
+        textAlign = prop?.value
         return this
     }
 
@@ -62,6 +67,18 @@ class AALabel {
 
     fun y(prop: Number?): AALabel {
         y = prop
+        return this
+    }
+
+    fun format(prop: String?): AALabel {
+        format = prop
+        return this
+    }
+
+    fun formatter(prop: String?): AALabel {
+        var pureJSFunctionStr = "($prop)"
+        pureJSFunctionStr = AAJSStringPurer.pureJavaScriptFunctionString(pureJSFunctionStr)
+        formatter = pureJSFunctionStr
         return this
     }
 
