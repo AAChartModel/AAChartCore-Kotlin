5e8e37c613b7d89ba491d5ef79a4ebbe88331afb
An An <2236368544@qq.com>
2022-09-28 18:55:57 +0800

    Update JSFunctionForAAChartEventsComposer.kt

--- sample/src/main/java/com/github/aachartmodel/aainfographics/demo/chartcomposer/JSFunctionForAAChartEventsComposer.kt
+++ sample/src/main/java/com/github/aachartmodel/aainfographics/demo/chartcomposer/JSFunctionForAAChartEventsComposer.kt
@@ -881,24 +881,8 @@ object JSFunctionForAAChartEventsComposer {
                     AASeriesElement()
                         .name("Virtual Data")
                         .lineWidth(6f)
-                        .data(arrayOf(
-                                7.0f,
-                                6.9f,
-                                2.5f,
-                                14.5f,
-                                18.2f,
-                                singleSpecialData,
-                                5.2f,
-                                26.5f,
-                                23.3f,
-                                45.3f,
-                                13.9f,
-                                9.6f
-                            )
-                        )
-                        .color(AAColor.Red)
-                )
-            )
+                        .data(arrayOf( 7.0f, 6.9f, 2.5f, 14.5f, 18.2f, singleSpecialData, 5.2f, 26.5f, 23.3f, 45.3f, 13.9f, 9.6f))
+                        .color(AAColor.Red)))
     }
 
     //https://github.com/AAChartModel/AAChartKit-Swift/issues/394
@@ -1041,20 +1025,7 @@ object JSFunctionForAAChartEventsComposer {
                     AASeriesElement()
                         .type(AAChartType.Areaspline)
                         .id("main")
-                        .data(arrayOf<Any>(
-                                7.0,
-                                6.9,
-                                2.5,
-                                14.5,
-                                18.2,
-                                21.5,
-                                5.2,
-                                26.5,
-                                23.3,
-                                45.3,
-                                13.9,
-                                9.6
-                            ))
+                        .data(arrayOf(7.0, 6.9, 2.5, 14.5, 18.2, 21.5, 5.2, 26.5, 23.3, 45.3, 13.9, 9.6))
                         .lineWidth(6)
                         .color("#dbe751")
                         .fillOpacity(0.4f)
