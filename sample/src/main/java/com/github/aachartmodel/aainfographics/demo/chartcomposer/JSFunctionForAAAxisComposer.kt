730e1f0cd52f97efce20ff028cab07de08759194
zhaoyan.li <2236368544@qq.com>
2022-10-16 15:43:36 +0800

    Update JSFunctionForAAAxisComposer.kt

--- sample/src/main/java/com/github/aachartmodel/aainfographics/demo/chartcomposer/JSFunctionForAAAxisComposer.kt
+++ sample/src/main/java/com/github/aachartmodel/aainfographics/demo/chartcomposer/JSFunctionForAAAxisComposer.kt
@@ -6,25 +6,6 @@ import com.github.aachartmodel.aainfographics.aatools.AAColor
 import com.github.aachartmodel.aainfographics.aatools.AAGradientColor
 import com.github.aachartmodel.aainfographics.aatools.AARgba
 
-
-//override func chartConfigurationWithSelectedIndex(_ selectedIndex: Int) -> Any? {
-//    switch (selectedIndex) {
-//        case 0: return customYAxisLabels()//自定义Y轴文字
-//        case 1: return customYAxisLabels2()//自定义Y轴文字2
-//        case 2: return customAreaChartXAxisLabelsTextUnitSuffix1()//自定义X轴文字单位后缀(通过 formatter 函数)
-//        case 3: return customAreaChartXAxisLabelsTextUnitSuffix2()//自定义X轴文字单位后缀(不通过 formatter 函数)
-//        case 4: return configureTheAxesLabelsFormattersOfDoubleYAxesChart()//配置双 Y 轴图表的 Y 轴文字标签的 Formatter 函数 示例 1
-//        case 5: return configureTheAxesLabelsFormattersOfDoubleYAxesChart2()//配置双 Y 轴图表的 Y 轴文字标签的 Formatter 函数 示例 2
-//        case 6: return configureTheAxesLabelsFormattersOfDoubleYAxesChart3()//配置双 Y 轴图表的 Y 轴文字标签的 Formatter 函数 示例 3
-//        case 7: return customColumnChartXAxisLabelsTextByInterceptTheFirstFourCharacters()//通过截取前四个字符来自定义 X 轴 labels
-//        case 8: return customSpiderChartStyle()//自定义蜘蛛🕷🕸图样式
-//        case 9: return customizeEveryDataLabelSinglelyByDataLabelsFormatter()//通过 DataLabels 的 formatter 函数来实现单个数据标签🏷自定义
-//        case 10: return customXAxisLabelsBeImages()//自定义 X轴 labels 为一组图片
-//        default:
-//        return nil
-//    }
-//}
-
 object JSFunctionForAAAxisComposer {
     fun customYAxisLabels(): AAOptions {
         val aaChartModel = AAChartModel()
