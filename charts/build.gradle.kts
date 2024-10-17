10c7f33ff8c729df09e4c671be1d205e5f739eb0
An An <2236368544@qq.com>
2024-10-17 09:49:32 +0800

    Update build.gradle.kts

--- charts/build.gradle.kts
+++ charts/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         targetSdkVersion(AndroidConstants.targetSdkVersion)
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
         jvmTarget = "1.8"
