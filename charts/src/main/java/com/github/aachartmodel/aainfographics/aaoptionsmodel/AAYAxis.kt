df3e43d19b56133828be9abba8ccbc74741246b2
zhaoyan.li <2236368544@qq.com>
2025-09-18 17:05:25 +0800

    Fix #256 => add `fun title(prop: AAAxisTitle): AAYAxis {...}` for AAYAxis

--- charts/src/main/java/com/github/aachartmodel/aainfographics/aaoptionsmodel/AAYAxis.kt
+++ charts/src/main/java/com/github/aachartmodel/aainfographics/aaoptionsmodel/AAYAxis.kt
@@ -36,7 +36,6 @@ open class AAYAxis: AAAxis() {
         return this
     }
 
-
     // 为了保持向后兼容性，添加对 AATitle 的支持
     // 同时添加方法废弃警告⚠️, 提示用户使用新的 AAAxisTitle 类型
     @Deprecated("Use `fun title(prop: AAAxisTitle): AAYAxis` instead. AATitle is deprecated for axis titles.")
