fbfa2fe9e9e3543d072f31574cc7820528929447
zhaoyan.li <2236368544@qq.com>
2025-09-22 14:05:10 +0800

    Update Gson version to 2.11.0
    Bumped the gson_version constant from 2.10.1 to 2.11.0

--- buildSrc/src/main/java/Constants.kt
+++ buildSrc/src/main/java/Constants.kt
@@ -15,7 +15,7 @@ object Versions {
 
     const val bintray_version = "1.8.5"
     const val kotlin_stdlib_version = "1.4.10"
-    const val gson_version = "2.10.1"
+    const val gson_version = "2.11.0"
 }
 
 object Libs {
